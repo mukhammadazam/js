370c4995251c3ff313eee7214af9acf76a955bee
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-01-22 04:17:02 +0300

    array and object

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ console.log(typeof hauseNamber);
 console.log(typeof isMarried);
 console.log(typeof age);
 console.log(typeof LName);
+const houseNanmber = 157;
+const tenant = "John Doe"; //tarjimasi odam
+
+// 3ta 4ta constlarni bitta joyga chiqarib berish -->
+console.log(tenant + "plus bilan qilish faqat eskicha yuli" + streetName + "men typealar ichiga oddiy suzlarniham qushib ketsam buladi js chiqariberuvradi bush joy bulsa ham" + hauseNamber);
+// `` <--- bag teg deyiladi
+//yangi s6 versiyada bunday yoziladi -->
+console.log(`${tenant} tashqaridan olgin degani ${streetName} shunaqa qilib tashqaridan oladi ${hauseNamber}`);
+console.log(tenant.toLowerCase()); // toLowerCase --->bu harflarni kichkina qilib beradi
+console.log(tenant.split()); // qizliga olib qushtirnoqqa olib beradi
+console.log(tenant.split('')); // bunday qilganda esa ['J', 'o', 'h', 'n', ' ', 'D', 'o', 'e'] bu xolga keltirib beradi
+console.log(tenant.split(' ')); //bunday bulganda ['John', 'Doe'] bush joydan bulib tashla degani
+const array = ["Audi", "BMW", 'Tesla']; //array bu spiska yani buni ichiga string quysam buladi data quysam buladi
+console.log(array); // Bu hozir oddiy 'array'
+array.push('Toyota'); // PUSH degani yangi data qush degani ketma ketlikda
+console.log(array);
+array.unshift('shevrolet'); // UNSHIFT bu boshiga qushib beradi bu kup ishlatilmaydi
+console.log(array);
+array.pop(); // POP bu oxiridan bitta datani olib tashlaydi umuman ishlatilmasa kerak
+console.log(array);
+// Object bu kalit suz va qiymatdan tshkil topgan yani misollar kuramiz ---->
+const person = {
+        name: 'Mukhammad azam',
+        isStudy: true,
+        address: {
+            street: "Main st",
+            hausenamber: 208,
+        }
+    } // bu endi murakkab arrayni kurdik bu object murakkabi edi qanday buni chiqarib berishni kuramiz
+console.log(person.name, person.address.hausenamber, person.isStudy)
