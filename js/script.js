f23024e34325c337fe0857c7513ecbafa5e0ad6a
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-01-22 03:17:52 +0300

    js type about

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let name = 'abdulloh';
+console.log(name);
+name = "boshqa";
+console.log(name);
+const anme = "rizayev";
+console.log(anme);
+// anme = '123'; ---> constga berilgan qiymatni uzgartirib bulmaydi
+//data type turlari
+// string, namber, boolean, null,undefined,object,array
+
+//string
+const streetName = "Prezident st";
+const hauseNamber = 208; //nomerlar uchun type namber
+const isMarried = false; // boolean ha yoki yuq deb janob beroadi rost yoki yolgon
+const age = null; // null bu data bor qiymatiyam bor lekin bush yani 0
+const LName = undefined; // undefined bu qiymat bor lekin bush yani const =LName shunday yozsayam buladi
+console.log(typeof streetName);
+console.log(typeof hauseNamber);
+console.log(typeof isMarried);
+console.log(typeof age);
+console.log(typeof LName);
